86e542581ce1b2dbdc7bddfe81ad40bc1705f06e
PKUltimate-Coder <1078313@students.wits.ac.za>
2019-03-14 19:38:27 +0200

    Third commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let express = require('express');
 let app = express();
 let mainrouter = require('./mainroutes');
     app.use(mainrouter);
+    
     let port = process.env.PORT || 3000;
     app.listen(port);
     console.log('Express server running on port', port);
